98d07ed15fc584dadd5368ce2ef786679e393e21
Nikita Zabashtin <nekitov.maksim@gmail.com>
2025-07-02 17:33:34 +0300

    ♻️ Рефактор входной точки по требованиям FSD

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,14 +1,25 @@
-import './assets/main.css'
+import './main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import { vueQueryPlugin } from './providers/vue-query'
 import App from './App.vue'
-import router from './App.vue'
+import { router } from './providers/router'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(vueQueryPlugin)
+app.use(vuetify)
 app.mount('#app')
