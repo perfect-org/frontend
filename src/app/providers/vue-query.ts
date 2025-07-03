9fbd8eac2ea28e2f7b37c1e6b88aaafd9151aadc
Nikita Zabashtin <nekitov.maksim@gmail.com>
2025-07-03 22:31:20 +0300

    🐛 Фикс настроек клиента для запросов

--- src/app/providers/vue-query.ts
+++ src/app/providers/vue-query.ts
@@ -1,6 +1,31 @@
 import { VueQueryPlugin, QueryClient } from '@tanstack/vue-query'
+import type { AxiosError } from 'axios'
 
-export const queryClient = new QueryClient()
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+      staleTime: 0,
+      gcTime: 0,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+    },
+    mutations: {
+      retry: false,
+      onError: (error) => {
+        const axiosError = error as AxiosError
+        if (axiosError.response?.status === 400) {
+          const errorData = axiosError.response.data as {
+            detail?: string
+          }
+          console.warn(errorData.detail)
+        }
+        throw error
+      },
+    },
+  },
+})
 
 export const vueQueryPlugin = {
   install(app) {
