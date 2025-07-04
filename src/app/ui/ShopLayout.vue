f803e2c88e31e4d3599bffd0bf49b3809e662870
Nikita Zabashtin <nekitov.maksim@gmail.com>
2025-07-04 21:02:43 +0300

    💄 Базовый layout магазина

---
+++ src/app/ui/ShopLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <Header />
+    <main class="main">
+      <RouterView />
+    </main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import Header from '@/widgets/Header.vue'
+import { RouterView } from 'vue-router'
+</script>
+
+<style lang="scss">
+.main {
+  height: calc(100vh - 64px);
+  margin-top: 64px;
+  padding: 2rem;
+}
+</style>
