dfd00ae01586985153afaca8060d058ee62c0e20
Nikita Zabashtin <nekitov.maksim@gmail.com>
2025-07-04 21:02:23 +0300

    🐛 Добавление компонента тоста во компонент входной точки

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { snackbar, snackbarText, snackbarColor } from '@/shared/lib/services/notification.service'
 </script>
 
 <template>
   <RouterView />
+  <v-snackbar
+    v-model="snackbar"
+    :color="snackbarColor"
+    :timeout="3000"
+    location="bottom right"
+    transition="slide-y-transition"
+  >
+    {{ snackbarText }}
+    <template v-slot:actions>
+      <v-btn color="white" variant="text" @click="snackbar = false">
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+    </template>
+  </v-snackbar>
 </template>
 
 <style scoped></style>
